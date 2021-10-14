03217df51a6985d097dc74f88ee9d56027852293
Vistar <email2vistarg@gmail.com>
2021-10-14 19:19:06 +0530

    used latest theme syntax from flutter 2.5.2

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(secondary: Colors.purple[300]),
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF0A0E21),
+        ),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('This is app bar'),
+      ),
+      body: const Center(
+        child: Text('This is body'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
